aa5eddacafbc5dc7ba490157692ecd1643cddc54
Samuel Zuniga Vanoye <szunigav@tibco-support.com>
2016-01-19 21:57:24 -0600

    Setup initial hapijs app, only index route.

--- source/server/app.js
+++ source/server/app.js
@@ -1,3 +1,27 @@
 'use strict';
 
 const Hapi  = require('hapi');
+
+const server = new Hapi.Server();
+const configuration = {
+    host: 'localhost',
+    port: 3000
+};
+
+server.connection(configuration);
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        return reply('Index page');
+    }
+});
+
+
+server.start((err) => {
+    if(err) {
+        throw error;
+    }
+    console.log('Server running at ', server.info.uri);
+});
+
