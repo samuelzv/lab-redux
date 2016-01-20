2497acdb34048f9d3199c3036bc207c48081f1ac
Samuel Zuniga Vanoye <szunigav@tibco-support.com>
2016-01-19 21:44:22 -0600

    Split grunt tasks for client and server elements.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,12 +8,17 @@ const newer   = require('gulp-newer');
 
 var appFiles = {
     html: [
-        'client/source/index.html'
+        'source/client/index.html'
     ],
-    source: [
-        'client/source/components/TodoApp.jsx',
-        'client/source/js/todos.js'
-    ],
+    source: {
+        client: [
+            'source/client/components/TodoApp.jsx',
+            'source/client/js/todos.js'
+        ],
+        server: [
+            'source/server/app.js'
+        ]
+    },
     vendor: [
         'bower_components/react/react.js',
         'node_modules/redux/dist/redux.js'
@@ -27,18 +32,28 @@ var onError = (error) => {
     console.log('****End Error****');
 };
 
-gulp.task('lint', () => {
-    return gulp.src(appFiles.source)
+
+var lintFiles = (source) => {
+     return gulp.src(source)
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError())
-});
+};
+
 
 var copyFiles = (source, destination) => {
     return gulp.src(source)
         .pipe(newer(destination))
         .pipe(gulp.dest(destination));
 };
+
+gulp.task('lint-client', () => {
+    return lintFiles(appFiles.source.client);
+});
+
+gulp.task('lint-server', () => {
+    return lintFiles(appFiles.source.server);
+});
 
 gulp.task('copy-files', ['copy-html','copy-vendor'], ()=> {
 });
@@ -51,16 +66,25 @@ gulp.task('copy-html', ()=> {
     return copyFiles(appFiles.html, 'distro/client');
 });
 
-gulp.task('concat', ['lint'], () => {
-    return gulp.src(appFiles.source)
+gulp.task('concat-client', ['lint-client'], () => {
+    return gulp.src(appFiles.source.client)
         .pipe(babel())
         .pipe(concat('all.js'))
         .pipe(gulp.dest('distro/client/js'));
 });
 
-
-gulp.task('build', ['copy-files', 'concat'], ()=> {
+gulp.task('translate-server', ['lint-server'], () => {
+    return gulp.src(appFiles.source.server)
+        .pipe(babel())
+        .pipe(gulp.dest('distro/server'));
 });
 
-gulp.task('default',['lint'], () => {
+
+gulp.task('build-client', ['copy-files', 'concat-client'], ()=> {
+});
+
+gulp.task('build-server', ['translate-server'], ()=> {
+});
+
+gulp.task('default',['build-client', 'build-server'], () => {
 });
